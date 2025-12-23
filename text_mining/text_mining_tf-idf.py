fb11454f265006ff13995d8fd576320dda5f8c8a
Matteo-glz <matteogalizia0407@gmail.com>
2025-12-23 12:02:10 +0100

    Update text_mining_tf-idf.py

--- text_mining/text_mining_tf-idf.py
+++ text_mining/text_mining_tf-idf.py
@@ -11,9 +11,14 @@ from load_corpus import load_corpus
 
 nltk.download("stopwords")
 nltk.download("punkt")
-custom_stopwords = {"company", "business", "report", "year", "page", "website",
-    "provide", "include", "information", "service", "client", "group,", "pdf", "site",
-    "download"}
+custom_stopwords = {
+    "company", "business", "report", "year", "page", "website", "provide", 
+    "include", "information", "service", "client", "group", "pdf", "site", 
+    "download", "consent", "cookie", "use", "data", "also", "may", "one", 
+    "new", "us", "cookies", "cooky", "privacy", "device", "browser",
+    "collect", "processing", "purpose", "request", "contact", "law", 
+    "advertising", "third", "party", "personal", "identify", "storage"
+}
 custom_stopwords = set(nltk.corpus.stopwords.words("english")).union(custom_stopwords)
 # =====================
 # LOAD CORPUS
